74a692f83b5c62e74f178581f464d61adcfe354c
lilingv <lilingv@84fa3890-7848-0410-87fb-3d64402ee7d4>
2009-04-05 16:54:23 +0000

    Adjust summary related files
    git-svn-id: https://bitoj.googlecode.com/svn/trunk/moodle/mod_programming@199 84fa3890-7848-0410-87fb-3d64402ee7d4

--- reports/summary.php
+++ reports/summary.php
@@ -2,7 +2,6 @@
 
     require_once('../../../config.php');
     require_once('../lib.php');
-    require_once('reports.lib.php');
     require_once($CFG->dirroot.'/lib/tablelib.php');
 
     $a = optional_param('a', 0, PARAM_INT);     // programming ID
@@ -48,6 +47,7 @@
     echo '<h1>'.get_string('summary', 'programming').'</h1>';
     print_summary_table($stat_results);
     print_summary_chart($stat_results);
+    print_action_table();
     echo '</div>';
 
 /// Finish the page
@@ -176,6 +176,134 @@ function print_summary_chart($stat_results) {
 
     }
 </script>";
+}
+
+/**
+ * 统计各个小组完成题目的情况。
+ *
+ * 目前此函数只处理 roleid 为 5 即学生的情况。
+ *
+ * @param $state_results 存储统计结果
+ * @param $group 要统计的小组，如果为 null 则统计全部人员的情况
+ */
+function summary_stat(&$stat_results, $group = null) {
+    global $USER, $CFG, $course, $programming;
+
+    $context = get_record('context', 'contextlevel', CONTEXT_COURSE, 'instanceid', $course->id);
+    $roleid = 5;
+
+    if ($group) {
+        $gfrom = ", {$CFG->prefix}groups_members AS gm";
+        $gwhere = " AND gm.groupid = $group->id AND ra.userid = gm.userid ";
+        $name = $group->name;
+    } else {
+        $gfrom = $gwhere = '';
+        $name = get_string('allstudents', 'programming', $course->students);
+    }
+
+    $studentcount = count_records_sql("
+        SELECT COUNT(*)
+          FROM {$CFG->prefix}role_assignments AS ra
+               $gfrom
+         WHERE ra.roleid = $roleid
+           AND ra.contextid = $context->id
+               $gwhere");
+    $submitcount = count_records_sql("
+        SELECT COUNT(*)
+          FROM {$CFG->prefix}role_assignments AS ra,
+               {$CFG->prefix}programming_result AS pr
+               $gfrom
+         WHERE ra.roleid = $roleid
+           AND ra.contextid = $context->id
+           AND pr.programmingid = $programming->id
+           AND ra.userid = pr.userid
+               $gwhere");
+    $compiledcount = count_records_sql("
+        SELECT COUNT(*)
+          FROM {$CFG->prefix}role_assignments AS ra,
+               {$CFG->prefix}programming_result AS pr,
+               {$CFG->prefix}programming_submits AS ps
+               $gfrom
+         WHERE ps.programmingid = $programming->id
+           AND pr.programmingid = $programming->id
+           AND ra.roleid = $roleid
+           AND ra.contextid = $context->id
+           AND ps.id = pr.latestsubmitid
+           AND pr.userid = ra.userid
+           AND ps.judgeresult != 'CE' AND ps.judgeresult != ''
+               $gwhere");
+    $passedcount = count_records_sql("
+        SELECT COUNT(*)
+          FROM {$CFG->prefix}role_assignments AS ra,
+               {$CFG->prefix}programming_submits AS ps,
+               {$CFG->prefix}programming_result AS pr
+               $gfrom
+         WHERE ps.programmingid = {$programming->id}
+           AND pr.programmingid = {$programming->id}
+           AND ra.roleid = $roleid
+           AND ra.contextid = $context->id
+           AND pr.userid = ra.userid
+           AND pr.latestsubmitid = ps.id
+           AND ps.passed = 1
+               $gwhere");
+    $intimepassedcount = count_records_sql("
+        SELECT COUNT(*)
+          FROM {$CFG->prefix}role_assignments AS ra,
+               {$CFG->prefix}programming_submits AS ps,
+               {$CFG->prefix}programming_result AS pr
+               $gfrom
+         WHERE ps.programmingid = {$programming->id}
+           AND pr.programmingid = {$programming->id}
+           AND ra.roleid = $roleid
+           AND ra.contextid = $context->id
+           AND pr.userid = ra.userid
+           AND pr.latestsubmitid = ps.id
+           AND ps.timemodified <= {$programming->timediscount}
+           AND ps.passed = 1
+               $gwhere");
+    $codeavg = count_records_sql("
+        SELECT AVG(codelines)
+          FROM {$CFG->prefix}role_assignments AS ra,
+               {$CFG->prefix}programming_submits AS ps,
+               {$CFG->prefix}programming_result AS pr
+               $gfrom
+         WHERE ps.programmingid = {$programming->id}
+           AND pr.programmingid = {$programming->id}
+           AND pr.latestsubmitid = ps.id
+           AND ra.roleid = $roleid
+           AND ra.contextid = $context->id
+           AND pr.userid = ra.userid
+               $gwhere");
+    array_push($stat_results,
+        array('name' => $name,
+              'studentcount' => $studentcount,
+              'submitcount' => $submitcount,
+              'compiledcount' => $compiledcount,
+              'passedcount' => $passedcount,
+              'intimepassedcount' => $intimepassedcount,
+              'averagelines' => $codeavg));
+    return;
+}
+
+function print_action_table() {
+    global $CFG, $programming, $context;
+
+    echo '<table class="generaltable" align="center">';
+    if (has_capability('mod/programming:viewotherprogram', $context)) {
+        echo '<tr><td>';
+        print_single_button($CFG->wwwroot.'/mod/programming/package.php', array('a' => $programming->id), get_string('package', 'programming'));
+        echo '</td><td>';
+        echo get_string('packagedesc', 'programming');
+        echo '</td></tr>';
+    }
+    if (has_capability('mod/programming:edittestcase', $context)) {
+        echo '<tr><td>';
+        print_single_button($CFG->wwwroot.'/mod/programming/rejudge.php', array('a' => $programming->id), get_string('rejudge', 'programming'));
+        echo '</td><td>';
+        echo get_string('retestdesc', 'programming');
+        echo '</td></tr>';
+    }
+    echo '</table>';
 }
 
 ?>
